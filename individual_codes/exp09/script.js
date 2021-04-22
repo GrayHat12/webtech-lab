926b52abd3eea09fdc23bd1a4b3946c11ae540d2
GrayHat12 <grayhathacks10@gmail.com>
2021-04-22 08:16:10 +0530

    exp09 update

--- individual_codes/exp09/script.js
+++ individual_codes/exp09/script.js
@@ -1,4 +1,10 @@
 let body = document.getElementsByTagName("body")[0];
+let val1 = document.getElementById("val1");
+let val2 = document.getElementById("val2");
+let x = document.getElementById("x");
+let mul = document.getElementById("mul");
+let div = document.getElementById("div");
+let clear = document.getElementById("clear");
 
 function removeClasses() {
     let currentClasses = Array.from(body.classList.values());
@@ -22,3 +28,47 @@ window.onhashchange = (ev => {
         body.classList.add("pink");
     }
 });
+
+function getValues() {
+    let v1 = parseFloat(val1.value);
+    let v2 = parseFloat(val2.value);
+    return [v1, v2];
+}
+
+mul.onclick = (ev) => {
+    ev.preventDefault();
+    try {
+        let [v1, v2] = getValues();
+        console.log(v1, v2);
+        let ans = v1 * v2;
+        if (isNaN(ans)) {
+            x.value = "Not a number supplied";
+        } else {
+            x.value = ans;
+        }
+    }
+    catch (err) {
+        alert("Invalid values");
+    }
+}
+div.onclick = (ev) => {
+    ev.preventDefault();
+    try {
+        let [v1, v2] = getValues();
+        console.log(v1, v2);
+        let ans = v1 / v2;
+        if (isNaN(ans)) {
+            x.value = "Not a number supplied";
+        } else {
+            x.value = ans;
+        }
+    }
+    catch (err) {
+        alert("Invalid values");
+    }
+}
+clear.onclick = (ev) => {
+    ev.preventDefault();
+    val1.value = 0;
+    val2.value = 0;
+}
